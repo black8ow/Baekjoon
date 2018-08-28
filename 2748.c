177f9c76f254fafa566c2298d5eab03f60539de7
black8ow <thecrooks2@gmail.com>
2018-08-28 15:20:06 +0900

    fibo with recursive

--- 2748.c
+++ 2748.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 int fibo(int num);
-int first_fibo = 0;
-int second_fibo = 1;
-int sum = 0;
+int result;
 
 int main(){
 
@@ -17,15 +15,11 @@ int main(){
 
 int fibo(int num){
   
-  if (num == 1) return first_fibo;
-  if (num == 2) return second_fibo;
+  if (num == 0) return 0;
+  if (num == 1) return 1;
 
-  int i;
-  for (i = num; i > 1; i--){
-    sum = first_fibo + second_fibo;
-    first_fibo = second_fibo;
-    second_fibo = sum;  
-  }
-  return sum;
+  else result = fibo(num - 2) + fibo(num - 1);
+
+  return result;
 }
 
